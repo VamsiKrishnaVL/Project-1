79f1549c094da3a1c2a52285a688a9ee6e118c75
VamsiKrishnaVL <vamsikrishna9686@gmail.com>
2026-08-07 17:49:17 +0530

    Delete Project-1 directory

--- Project-1/pages/sample_page.py
+++
@@ -1,4 +0,0 @@
-from .base_page import BasePage
-
-class SamplePage(BasePage):
-    pass
--- Project-1/tests/test_sample.py
+++
@@ -1,3 +0,0 @@
-def test_example(driver):
-    driver.get('https://example.com')
-    assert 'Example' in driver.title
